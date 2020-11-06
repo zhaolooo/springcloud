2222b54dc6dba3688a95ba93ae11ded894062deb
tarena <zl623824@163.com>
2020-11-06 14:17:19 +0800

    spring cloud 2020/11/6

--- sp01-commons/src/main/java/cn/tedu/sp01/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package cn.tedu.sp01.service;
-
-import cn.tedu.sp01.pojo.User;
-
-public interface UserService {
-    User getUser(Integer userId);
-
-    void addScore(Integer userId, Integer score);
-}
