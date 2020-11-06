318da1dd9f84257f0837e0d1894df5fd7a9c1d58
tarena <zl623824@163.com>
2020-11-06 14:44:49 +0800

    di

---
+++ sp06-ribbon/src/main/java/cn/tedu/sp06/Sp06RibbonApplication.java
@@ -0,0 +1,32 @@
+package cn.tedu.sp06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+ 
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Sp06RibbonApplication {
+
+	//创建RestTemplate 实例，并存入 spring 容器
+	@LoadBalanced  //负载均衡的注解
+	@Bean
+	public RestTemplate getRestTemplate() {
+		SimpleClientHttpRequestFactory f = new SimpleClientHttpRequestFactory();
+		f.setConnectTimeout(1000);
+		f.setReadTimeout(1000);
+		
+		return new RestTemplate(f);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Sp06RibbonApplication.class, args);
+	}
+
+}
